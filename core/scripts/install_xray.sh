0b2fd6d5e72555328a9e8f44d27823adb5d24ac7
KRYYYYYYYYYYYYYYYYYYY <135027183+KRYYYYYYYYYYYYYYYYYYY@users.noreply.github.com>
2026-04-06 00:28:58 +0400

    Update install_xray.sh

--- core/scripts/install_xray.sh
+++ core/scripts/install_xray.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-set -e
-# Получаем последнюю версию
+# Скачиваем в корень, распаковываем в ./core/
+mkdir -p ./core
 VERSION=$(curl -s https://github.com | grep tag_name | cut -d '"' -f 4)
-echo "Downloading Xray $VERSION..."
 URL="https://github.com{VERSION}/Xray-linux-64.zip"
 
 curl -L -o xray.zip "$URL"
 unzip -o xray.zip -d ./core
 chmod +x ./core/xray
-echo "Xray installed to ./core/xray"
